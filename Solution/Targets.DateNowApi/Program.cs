e60f61d8653dd2b1d238e040e251bb49a92e5822
Антон Широких <anton.black777@gmail.com>
2022-05-07 19:13:02 +0300

    add endpoint for demonstrating empty query parameters, add test scenario with string concatenation

--- Solution/Targets.DateNowApi/Program.cs
+++ Solution/Targets.DateNowApi/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
@@ -7,4 +9,36 @@ app.MapGet("/datenow", () =>
     return DateTime.UtcNow.ToLongTimeString();
 });
 
+app.MapGet("/daysAgo", (StringArray myParameter) =>
+{
+    if (myParameter.Array.Any(x => string.IsNullOrEmpty(x)))
+        return Results.BadRequest();
+
+    var dateNow = DateTime.UtcNow;
+    var results = myParameter.Array.Select(x =>
+    {
+        var canParse = DateTime.TryParse(x, out var dateParsed);
+        return (canParse, dateParsed);
+    })
+    .Where(x => x.canParse)
+    .Select(x => (dateNow - x.dateParsed).Days.ToString());
+    return Results.Ok(results);
+});
+
 app.Run();
+
+
+
+public class StringArray
+{
+    public string[] Array { get; private set; }
+
+    public static ValueTask<StringArray> BindAsync(HttpContext context,
+                                                   ParameterInfo parameter)
+    {
+        var parameterKey = parameter.Name;
+
+        var stringValues = context.Request.Query[parameterKey!].ToArray();
+        return ValueTask.FromResult(new StringArray { Array = stringValues });
+    }
+}
--- Solution/Utils/ScenariousHelper.cs
+++ Solution/Utils/ScenariousHelper.cs
@@ -24,5 +24,42 @@ namespace Utils
 
             return scenario;
         }
+
+        public static Scenario GetDaysAgoTestScenario(HttpClient httpClient)
+        {
+            var callDateNowStep = Step.Create("call datenow", async (context) =>
+            {
+                var queryString = GetQueryString();
+                var response = await httpClient.GetAsync($"datenow{queryString}");
+                if (response.IsSuccessStatusCode)
+                    return Response.Ok(statusCode: (int)response.StatusCode);
+                else
+                    return Response.Fail(statusCode: (int)response.StatusCode);
+            });
+
+            var scenario = ScenarioBuilder.CreateScenario("Call DateNow Api", callDateNowStep)
+                .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+                .WithLoadSimulations(
+                    LoadSimulation.NewInjectPerSec(_rate: 100, _during: TimeSpan.FromSeconds(30))
+                );
+
+            return scenario;
+        }
+
+        private static string GetQueryString()
+        {
+            var parametersCount = Random.Shared.Next(0, 3);
+            if (parametersCount == 0)
+                return string.Empty;
+            var parametersString = "?";
+            for(int i =0; i< parametersCount-1; i++)
+            {
+                if (i != 0)
+                    parametersString += "&";
+                parametersString += $"myParameter={DateTime.Now}";
+            }
+
+            return parametersString;
+        }
     }
 }

f15c4cda58ff25df27622c79a8cdab28f190b4d7
Антон Широких <anton.black777@gmail.com>
2022-05-07 19:32:31 +0300

    add AutoBogus demonstration

---
+++ Solution/Cases.xUnitInMemoryTest/AutoBogusTests.cs
@@ -0,0 +1,34 @@
+﻿using AutoBogus;
+using AutoBogus.Conventions;
+using Xunit;
+
+namespace Cases.xUnitInMemoryTest
+{
+    public class AutoBogusTests
+    {
+        static AutoBogusTests()
+        {
+            AutoFaker.Configure(builder =>
+            {
+                builder.WithConventions();
+            });
+        }
+
+        internal class User
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+        }
+
+        [Fact(Skip = "For dev demonstration only")]
+        public void GenerateUser()
+        {
+            var user = AutoFaker.Generate<User>();
+            
+            Assert.NotEmpty(user.FirstName);
+            Assert.NotEmpty(user.LastName);
+            Assert.NotEmpty(user.Email);
+        }
+    }
+}
